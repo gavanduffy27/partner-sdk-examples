28eaa2dd814455b666c6ef9cb3881beca7766af9
gavan.duffy <gavan.duffy27@gmail.com>
2025-12-03 00:17:45 +0100

    Updates for config

--- partner-examples/dgie-examples/src/main/java/com/genkey/partner/example/PartnerTestSuite.java
+++ partner-examples/dgie-examples/src/main/java/com/genkey/partner/example/PartnerTestSuite.java
@@ -15,77 +15,78 @@ import com.genkey.platform.utils.FileUtils;
 
 public class PartnerTestSuite extends ExampleSuite {
 
-  public static boolean flgInit = false;
+	public static boolean flgInit = false;
 
-  public static boolean flgClientInit = false;
+	public static boolean flgClientInit = false;
 
-  // Define a settings file
-  // static String SETTINGS_FILE="./test/remoteTest.ini";
-  static String SETTINGS_FILE = "./test/localTest.ini";
+	// Define a settings file
+	// static String SETTINGS_FILE="./test/remoteTest.ini";
+	static String SETTINGS_FILE = "./test/localTest.ini";
 
-  // static PropertyMap prop
+	// static PropertyMap prop
 
-  public static void init() {
-    if (!flgInit) {
-      try {
-        // Comment out this line to use other mechanisms
-        //ExampleTestUtils.setSettingsFile(SETTINGS_FILE);
+	public static void init() {
+		if (!flgInit) {
+			try {
+				// Comment out this line to use other mechanisms
+				// ExampleTestUtils.setSettingsFile(SETTINGS_FILE);
 
-        // Generic method for establishing settings
-        ExampleTestUtils.loadDefaultSettings();
+				// Generic method for establishing settings
+				ExampleTestUtils.setCodeDefaultSettings();
+				ExampleTestUtils.loadDefaultSettings();
 
-        String ServiceHost = PartnerExample.getPrimaryHost();
-        int ServicePort = PartnerExample.getServicePort();
-        String DomainName = PartnerExample.getPartnerDomainName();
-        String LegacyServiceHost = PartnerExample.getLegacyHost();
-        String DrServer = PartnerExample.getSecondaryHost();
+				String ServiceHost = PartnerExample.getPrimaryHost();
+				int ServicePort = PartnerExample.getServicePort();
+				String DomainName = PartnerExample.getPartnerDomainName();
+				String LegacyServiceHost = PartnerExample.getLegacyHost();
+				String DrServer = PartnerExample.getSecondaryHost();
 
-        // ABISClientLibrary.initializeWithSettings(SettingsFile);
-        RestServices.getInstance().setABISPort(0);
-        ABISClientLibrary.initializeDefault();
-        DGIEServiceModule.initCoreServices(ServiceHost, ServicePort, DomainName);
-        DGIEServiceModule.initLegacyService(LegacyServiceHost, ServicePort);
-        DGIEServiceModule.setFailoverServer(DrServer);
-        // Performs stand-alone check on startup
-        RestServices.getInstance().checkFailover(true);
+				// ABISClientLibrary.initializeWithSettings(SettingsFile);
+				RestServices.getInstance().setABISPort(0);
+				ABISClientLibrary.initializeDefault();
+				DGIEServiceModule.initCoreServices(ServiceHost, ServicePort, DomainName);
+				DGIEServiceModule.initLegacyService(LegacyServiceHost, ServicePort);
+				DGIEServiceModule.setFailoverServer(DrServer);
+				// Performs stand-alone check on startup
+				RestServices.getInstance().checkFailover(true);
 
-        String cfgPath = ImageContextSDK.getConfigurationPath();
-        FileUtils.setConfigurationPath(cfgPath);
-      } catch (Exception e) {
-        handleException(e);
-      }
-      flgInit = true;
-    }
-  }
+				String cfgPath = ImageContextSDK.getConfigurationPath();
+				FileUtils.setConfigurationPath(cfgPath);
+			} catch (Exception e) {
+				handleException(e);
+			}
+			flgInit = true;
+		}
+	}
 
-  public static void initClient() {
+	public static void initClient() {
 
-    if (!flgClientInit) {
-      try {
-        ABISClientLibrary.initializeDefault();
-        String cfgPath = ImageContextSDK.getConfigurationPath();
-        FileUtils.setConfigurationPath(cfgPath);
+		if (!flgClientInit) {
+			try {
+				ABISClientLibrary.initializeDefault();
+				String cfgPath = ImageContextSDK.getConfigurationPath();
+				FileUtils.setConfigurationPath(cfgPath);
 
-      } catch (Exception e) {
-        handleException(e);
-      }
-    }
-  }
+			} catch (Exception e) {
+				handleException(e);
+			}
+		}
+	}
 
-  @Override
-  protected void initExampleSuite() {
-    init();
-  }
+	@Override
+	protected void initExampleSuite() {
+		init();
+	}
 
-  public static GenkeyABISService getABISService() {
-    return ABISServiceModule.getABISService();
-  }
+	public static GenkeyABISService getABISService() {
+		return ABISServiceModule.getABISService();
+	}
 
-  public static BiographicService getBiographicService() {
-    return BiographicServiceModule.getBiographicService();
-  }
+	public static BiographicService getBiographicService() {
+		return BiographicServiceModule.getBiographicService();
+	}
 
-  public static LegacyMatchingService getLegacyService() {
-    return DGIEServiceModule.getLegacyService();
-  }
+	public static LegacyMatchingService getLegacyService() {
+		return DGIEServiceModule.getLegacyService();
+	}
 }
--- partner-examples/example-framework/src/main/java/com/genkey/abisclient/examples/utils/ExampleTestUtils.java
+++ partner-examples/example-framework/src/main/java/com/genkey/abisclient/examples/utils/ExampleTestUtils.java
@@ -6,86 +6,119 @@ import org.slf4j.LoggerFactory;
 
 public class ExampleTestUtils {
 
-  static org.slf4j.Logger logger = LoggerFactory.getLogger(ExampleTestUtils.class);
+	static org.slf4j.Logger logger = LoggerFactory.getLogger(ExampleTestUtils.class);
 
-  public static String DefaultSettingsFile = "partnerExample.ini";
-  public static String OPT_SETTINGS = "abis.partner.settings_file";
+	public static String DefaultSettingsFile = "partnerExample.ini";
+	public static String OPT_SETTINGS = "abis.partner.settings_file";
+	public static String OPT_CODE_DEFAULT = "abis.partner.useCodeDefault";
 
-  public static void setSettingsFile(String fileName) {
-    System.setProperty(OPT_SETTINGS, fileName);
-  }
+	static String CODE_SETTINGS_FILE = "./test/localTest.ini";
 
-  public static void loadDefaultSettings() {
-    loadSettings(OPT_SETTINGS, DefaultSettingsFile);
-  }
+	public static void setSettingsFile(String fileName) {
+		System.setProperty(OPT_SETTINGS, fileName);
+	}
 
-  /**
-   * Loads property settings file
-   *
-   * @param settingFile
-   * @param defaultFileName
-   */
-  public static void loadSettings(String settingFile, String defaultFileName) {
-    String fileName = getPropertyValue(settingFile, defaultFileName);
-    if (FileUtils.existsFile(fileName)) {
-      try {
-        FileInputStream fis = new FileInputStream(fileName);
-        System.getProperties().load(fis);
-      } catch (Exception e) {
-        logger.error("Failed on loading properties from {}", fileName);
-      }
-    } else {
-      if (!fileName.equals(defaultFileName)) {
-        logger.info("Specified settings file {} not found", fileName);
-      }
-    }
-  }
+	public static void loadDefaultSettings() {
+		loadSettings(OPT_SETTINGS, DefaultSettingsFile);
+	}
 
-  public static String asEnv(String property) {
-    String modValue = StringUtils.replaceAnyOfBy(property, ".", '_');
-    return modValue.toUpperCase();
-  }
+	/**
+	 * Loads property settings file
+	 *
+	 * @param settingFile
+	 * @param defaultFileName
+	 */
+	public static void loadSettings(String settingFile, String defaultFileName) {
+		String fileName = getPropertyValue(settingFile, defaultFileName);
+		if (FileUtils.existsFile(fileName)) {
+			try {
+				FileInputStream fis = new FileInputStream(fileName);
+				System.getProperties().load(fis);
+			} catch (Exception e) {
+				logger.error("Failed on loading properties from {}", fileName);
+			}
+		} else {
+			if (!fileName.equals(defaultFileName)) {
+				logger.info("Specified settings file {} not found", fileName);
+			}
+		}
+	}
 
-  /**
-   * Obtains a property name using the following precedence :
-   *
-   * <ol>
-   *   <li>Use system property if defined
-   *   <li>Use equivalent ENV value formed by substituting _ for . and capitalisation
-   *   <li>Accept the defaultValue
-   * </ol>
-   *
-   * @param propertyName
-   * @param defaultValue
-   * @return
-   */
-  public static String getPropertyValue(String propertyName, String defaultValue) {
-    String value = System.getProperty(propertyName);
-    if (value == null) {
-      value = getPropertyENV(propertyName);
-      if (value == null) {
-        value = defaultValue;
-      }
-    }
-    return value;
-  }
+	public static String asEnv(String property) {
+		String modValue = StringUtils.replaceAnyOfBy(property, ".", '_');
+		return modValue.toUpperCase();
+	}
 
-  public static String getPropertyENV(String propertyName) {
-    String env = asEnv(propertyName);
-    return System.getenv(env);
-  }
+	/**
+	 * Obtains a property name using the following precedence :
+	 *
+	 * <ol>
+	 * <li>Use system property if defined
+	 * <li>Use equivalent ENV value formed by substituting _ for . and
+	 * capitalisation
+	 * <li>Accept the defaultValue
+	 * </ol>
+	 *
+	 * @param propertyName
+	 * @param defaultValue
+	 * @return
+	 */
+	public static String getPropertyValue(String propertyName, String defaultValue) {
+		String value = System.getProperty(propertyName);
+		if (value == null) {
+			value = getPropertyENV(propertyName);
+			if (value == null) {
+				value = defaultValue;
+			}
+		}
+		return value;
+	}
 
-  public static int getPropertyInteger(String propertyName, int defaultValue) {
-    String value = getPropertyValue(propertyName, null);
-    int result = defaultValue;
-    if (value != null) {
-      try {
-        result = Integer.parseInt(value);
-      } catch (Exception e) {
-        logger.error("Failure on parse int for {}", value);
-        result = defaultValue;
-      }
-    }
-    return result;
-  }
+	public static String getPropertyENV(String propertyName) {
+		String env = asEnv(propertyName);
+		return System.getenv(env);
+	}
+
+	public static boolean getPropertyBoolean(String propertyName, boolean defaultValue) {
+		String value = getPropertyValue(propertyName, null);
+		boolean result = defaultValue;
+		if (value != null) {
+			try {
+				result = StringUtils.parseBoolean(value);
+			} catch (Exception e) {
+				logger.error("Failure on parse boolean for {}", value);
+				result = defaultValue;
+			}
+		}
+		return result;
+	}
+
+	public static int getPropertyInteger(String propertyName, int defaultValue) {
+		String value = getPropertyValue(propertyName, null);
+		int result = defaultValue;
+		if (value != null) {
+			try {
+				result = Integer.parseInt(value);
+			} catch (Exception e) {
+				logger.error("Failure on parse int for {}", value);
+				result = defaultValue;
+			}
+		}
+		return result;
+	}
+
+	public static boolean isUseCodeDefault() {
+		return getPropertyBoolean(OPT_CODE_DEFAULT, false);
+	}
+
+	public static void setCodeDefaultSettings() {
+		setCodeDefaultSettings(CODE_SETTINGS_FILE);
+	}
+
+	public static void setCodeDefaultSettings(String settingsFile) {
+		if (ExampleTestUtils.isUseCodeDefault()) {
+			ExampleTestUtils.setSettingsFile(settingsFile);
+		}
+	
+	
 }
