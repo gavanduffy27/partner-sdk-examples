1c21634aed50693bff0960383621f25f9ef2a816
gavan.duffy <gavan.duffy@genkey.com>
2026-01-13 21:27:33 +0000

    Update example for face-quality

--- partner-examples/dgie-examples/src/main/java/com/genkey/partner/example/functional/FaceRequestExamples.java
+++ partner-examples/dgie-examples/src/main/java/com/genkey/partner/example/functional/FaceRequestExamples.java
@@ -1,5 +1,6 @@
 package com.genkey.partner.example.functional;
 
+import com.genkey.abisclient.ImageBlob;
 import com.genkey.abisclient.service.ABISServiceModule;
 import com.genkey.abisclient.service.GenkeyABISService;
 import com.genkey.abisclient.service.MatchEngineResponse;
@@ -13,114 +14,118 @@ import com.genkey.partner.utils.EnrollmentUtils;
 
 public class FaceRequestExamples extends FunctionalTestExample {
 
-  public static int EnrollmentSubject = 1;
-  public static String EnrollmentSubjectID = String.valueOf(EnrollmentSubject);
+	public static int EnrollmentSubject = 1;
+	public static String EnrollmentSubjectID = String.valueOf(EnrollmentSubject);
 
-  public static void main(String[] args) {
-    PartnerExample test = new FaceRequestExamples();
-    test.processCommandLine(args);
-  }
+	public static void main(String[] args) {
+		PartnerExample test = new FaceRequestExamples();
+		test.processCommandLine(args);
+	}
 
-  public void setUp() {
-    super.setUp();
-    String abisConnection = getTestABISService().waitABISConnection(20000, 2000);
-    printObject("Connection Details", abisConnection);
-    TestABISService abisService = this.getTestABISService();
-    abisService.setApplyDomain(true);
-  }
+	public void setUp() {
+		super.setUp();
+		String abisConnection = getTestABISService().waitABISConnection(20000, 2000);
+		printObject("Connection Details", abisConnection);
+		TestABISService abisService = this.getTestABISService();
+		abisService.setApplyDomain(true);
+	}
 
-  @Override
-  protected void runAllExamples() {
-    verifyExample();
-    identifyExample();
-  }
+	@Override
+	protected void runAllExamples() {
+		verifyExample();
+		identifyExample();
+	}
 
-  public void verifyExample() {
-    verifyExample(EnrollmentSubjectID);
-  }
+	public void verifyExample() {
+		verifyExample(EnrollmentSubjectID);
+	}
 
-  public void verifyExample(String biographicId) {
-    SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
-    GenkeyABISService abisService = ABISServiceModule.getABISService();
-    if (!abisService.existsSubject(biographicId)) {
-      return;
-    }
-    VerifyResponse verifyResponse = abisService.verifySubject(enrollmentRef);
-    if (!verifyResponse.isSuccess()) {
-      handleRESTFailure(enrollmentRef, verifyResponse, abisService);
-    } else if (!verifyResponse.isVerified()) {
-      printMessage(
-          "Verification failure with score " + verifyResponse.getMatchResult().getMatchScore());
-      handleVerificationFailure(enrollmentRef, verifyResponse);
-    } else {
-      printMessage(
-          "Verification success with score " + verifyResponse.getMatchResult().getMatchScore());
-      displayMatchResult(verifyResponse.getMatchResult());
-    }
-  }
+	public void verifyExample(String biographicId) {
+		SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
+		GenkeyABISService abisService = ABISServiceModule.getABISService();
+		if (!abisService.existsSubject(biographicId)) {
+			return;
+		}
+		VerifyResponse verifyResponse = abisService.verifySubject(enrollmentRef);
+		if (!verifyResponse.isSuccess()) {
+			handleRESTFailure(enrollmentRef, verifyResponse, abisService);
+		} else if (!verifyResponse.isVerified()) {
+			printMessage("Verification failure with score " + verifyResponse.getMatchResult().getMatchScore());
+			handleVerificationFailure(enrollmentRef, verifyResponse);
+		} else {
+			printMessage("Verification success with score " + verifyResponse.getMatchResult().getMatchScore());
+			displayMatchResult(verifyResponse.getMatchResult());
+		}
+	}
 
-  public void updateExample() {
-	    faceUpdateExample(EnrollmentSubjectID);
-	  }
-  
-  
-  public void faceUpdateExample(String biographicId) {
-	    SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
-	    GenkeyABISService abisService = ABISServiceModule.getABISService();
-	    if (!abisService.existsSubject(biographicId)) {
-	      return;
-	    }
-	    UpdateResponse updateResponse = abisService.updateSubject(enrollmentRef);
-	    if (!updateResponse.isSuccess()) {
-	      handleRESTFailure(enrollmentRef, updateResponse, abisService);
-	    } else {
-	      printMessage(
-	          "Update success with " + updateResponse.getOperationResult());
-	    }
-	  }
-  
-  
-  public void identifyExample() {
-    identifyExample(EnrollmentSubjectID);
-  }
+	public void updateExample() {
+		faceUpdateExample(EnrollmentSubjectID);
+	}
 
-  public void identifyExample(String biographicId) {
-    SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
-    GenkeyABISService abisService = ABISServiceModule.getABISService();
-    /*
-    if (! abisService.existsSubject(biographicId)) {
-    	return;
-    }
-    */
-    MatchEngineResponse response = abisService.querySubject(enrollmentRef, false);
-    if (!response.isSuccess()) {
-      printMessage(
-          "Handling REST response failure on query of "
-              + enrollmentRef.getRequestID()
-              + " with error code "
-              + response.getStatusCode());
-      handleRESTFailure(enrollmentRef, response, abisService);
-      return;
-    } else {
-      printResult("MatchEngineResponse", response.getOperationResult());
-    }
+	public void faceUpdateExample(String biographicId) {
+		SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
+		GenkeyABISService abisService = ABISServiceModule.getABISService();
+		if (!abisService.existsSubject(biographicId)) {
+			return;
+		}
+		UpdateResponse updateResponse = abisService.updateSubject(enrollmentRef);
+		if (!updateResponse.isSuccess()) {
+			handleRESTFailure(enrollmentRef, updateResponse, abisService);
+		} else {
+			printMessage("Update success with " + updateResponse.getOperationResult());
+		}
+	}
 
-    BiographicProfileRecord biographicRecord =
-        EnrollmentUtils.getBiographicRecord(biographicId, "john", "doe");
+	public void identifyExample() {
+		identifyExample(EnrollmentSubjectID);
+	}
 
-    // Check for matches found and display
-    if (response.hasMatchResults()) {
-      handleMatchResults(biographicRecord, response);
-    }
-  }
+	public void identifyExample(String biographicId) {
+		SubjectEnrollmentReference enrollmentRef = faceOnlyEnroll(biographicId);
+		GenkeyABISService abisService = ABISServiceModule.getABISService();
+		/*
+		 * if (! abisService.existsSubject(biographicId)) { return; }
+		 */
+		MatchEngineResponse response = abisService.querySubject(enrollmentRef, false);
+		if (!response.isSuccess()) {
+			printMessage("Handling REST response failure on query of " + enrollmentRef.getRequestID()
+					+ " with error code " + response.getStatusCode());
+			handleRESTFailure(enrollmentRef, response, abisService);
+			return;
+		} else {
+			printResult("MatchEngineResponse", response.getOperationResult());
+		}
 
-  @Override
-  void doVerifyExample(String biographicId, int[] targetFingers) {}
+		BiographicProfileRecord biographicRecord = EnrollmentUtils.getBiographicRecord(biographicId, "john", "doe");
 
-  public SubjectEnrollmentReference faceOnlyEnroll(String biographicId) {
-    SubjectEnrollmentReference subject = new SubjectEnrollmentReference();
-    subject.setSubjectID(biographicId);
-    EnrollmentUtils.enrollFacePortrait(subject);
-    return subject;
-  }
+		// Check for matches found and display
+		if (response.hasMatchResults()) {
+			handleMatchResults(biographicRecord, response);
+		}
+	}
+
+	@Override
+	void doVerifyExample(String biographicId, int[] targetFingers) {
+	}
+
+	public void faceQualityCheckExample() {
+		faceQualityCheckExample(EnrollmentSubjectID);
+	}
+
+	public void faceQualityCheckExample(String biographicId) {
+		SubjectEnrollmentReference subject = new SubjectEnrollmentReference();
+		subject.setSubjectID(biographicId);
+		EnrollmentUtils.enrollFacePortrait(subject);
+		GenkeyABISService abisService = ABISServiceModule.getABISService();
+		ImageBlob blob = subject.getFacePortrait();
+		double score = abisService.getFaceImageQuality(blob);
+		printResult("Quality Score", score);
+	}
+
+	public SubjectEnrollmentReference faceOnlyEnroll(String biographicId) {
+		SubjectEnrollmentReference subject = new SubjectEnrollmentReference();
+		subject.setSubjectID(biographicId);
+		EnrollmentUtils.enrollFacePortrait(subject);
+		return subject;
+	}
 }
